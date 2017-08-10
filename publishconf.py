96f559e126d0b574c98d8915c936b950b406abbf
Duncan Temple Lang <duncan@r-project.org>
2017-08-09 17:39:45 -0700

    siteurl moved to dsi

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://karenyyng.github.io/something_nice'
+SITEURL = 'http://dsi.ucdavis.edu'
 OUTPUT_PATH = "build/"
 
 RELATIVE_URLS = False
